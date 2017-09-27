c067d0cc214d3a52c7fc3f4536f78b3f5c2e7f9f
Miguel A. Magjistari <Arber@iMac-de-Miguel.local>
2017-09-27 20:28:05 +0200

    Añadido método nombreLatino y todos los atributos necesarios

--- src/main/java/es/upm/miw/forge/ecp2/User.java
+++ src/main/java/es/upm/miw/forge/ecp2/User.java
@@ -6,16 +6,19 @@ public class User {
     private String name;
 
     private String familyName;
+    
+    private String secondFamilyName;
 
     private String format(String string) {
         string = string.trim();
         return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
     }
 
-    public User(int number, String name, String familyName) {
+    public User(int number, String name, String familyName, String secondFamilyName) {
         this.number = number;
         this.name = this.format(name);
         this.familyName = this.format(familyName);
+        this.secondFamilyName = this.format(secondFamilyName);
     }
 
     public int getNumber() {
@@ -29,9 +32,17 @@ public class User {
     public String getFamilyName() {
         return this.familyName;
     }
+    
+    public String getSecondFamilyName() {
+        return this.secondFamilyName;
+    }
 
     public String fullName() {
         return this.name + " " + this.familyName;
+    }
+    
+    public String nombreLatino() {
+        return this.name + " " + this.familyName + " " + this.secondFamilyName;
     }
 
     public String initials() {
